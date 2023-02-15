6e37137d9a35624ebab810f0f51ac619eb9ccc46
Joe Kwiatkowski <joejk2@gmail.com>
2023-02-15 11:07:57 +0000

    Convert `zl` and `zt` to functions

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,6 +1,12 @@
-### Source these scripts in order that they can set environment variable 'zp' 
+### These scripts are 'sourced' in order to set envvar 'zp' 
 alias zg="source $ZETTELKASTEN_SOURCE/bin/_zg "
-alias zl="source $ZETTELKASTEN_SOURCE/bin/_zl list_by_uid "
 alias zm="source $ZETTELKASTEN_SOURCE/bin/_zm "
 alias zn="source $ZETTELKASTEN_SOURCE/bin/_zn "
-alias zt="source $ZETTELKASTEN_SOURCE/bin/_zl list_by_priority "
+
+function zl() {
+    source $ZETTELKASTEN_SOURCE/bin/_zl list_by_uid "$@"
+}
+
+function zt() { # for 'zettelkasten tasks'
+    source $ZETTELKASTEN_SOURCE/bin/_zl list_by_priority "$@"
+}
