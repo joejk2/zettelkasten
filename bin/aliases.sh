503c78963a938dc686cd0ee1e16050ec532afdff
Joe Kwiatkowski <joejk2@gmail.com>
2023-02-28 10:04:26 +0000

    add command-line function "zs"

--- bin/aliases.sh
+++ bin/aliases.sh
@@ -1,10 +1,15 @@
-### These scripts are 'sourced' in order to set envvar 'zp' 
+# Scripts are 'sourced' in order to set envvar 'zp' 
 alias zg="source $ZETTELKASTEN_SOURCE/bin/_zg "
 alias zm="source $ZETTELKASTEN_SOURCE/bin/_zm "
 alias zn="source $ZETTELKASTEN_SOURCE/bin/_zn "
 
 function zl() {
     source $ZETTELKASTEN_SOURCE/bin/_zl list_by_uid "$@"
+}
+
+function zs() { # for 'zettelkasten subset' - generally run after 'zl'
+    prefix=`echo $zp | cut -d "-" -f 1`
+    source $ZETTELKASTEN_SOURCE/bin/_zl list_by_uid $prefix
 }
 
 function zt() { # for 'zettelkasten tasks'
